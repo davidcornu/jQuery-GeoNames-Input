e818286de8c70d0213c480b0856d58035b2e277a
David Cornu <davidjcornu@gmail.com>
2011-02-28 18:54:43 -0500

    Complete rewrite to use jQuery UI's autocomplete functionality and ensure only valid user input. Also added initial IP-based value using Maxmind GeoIP.

--- javascripts/jquery.geonames_input.js
+++ javascripts/jquery.geonames_input.js
@@ -5,50 +5,64 @@
  * Written by David Cornu.
  */
 
-
-//Fetch Information from GeoNames.org 
-function GetGeoNames(id, locale){
-	input = $("#" + id);
-	list = $("#" + id + "_suggestion_list");
-	base_url = "http://ws.geonames.org/searchJSON?";
-	limit_param = "maxRows=5";
-	locale_param = "lang=" + locale;
-	type_param = "featureClass=P"
-	search_param = "name=" + encodeURIComponent(input.val());
-	final_url = base_url + "&" + limit_param + "&" + locale_param + "&" + type_param + "&" + search_param;
-	if (input.val()){
-		input.addClass("loading");
-		$.getJSON(final_url, function(data){
-			input.removeClass("loading");
-			list.empty();
-			$.each(data.geonames, function(index, obj){
-				list.append("<li>" + obj.toponymName + ", " + obj.adminName1 + ", " + obj.countryName + "</li>");
-			});
-			list.slideDown('fast');
-		});
-	}
-}
-
-//Handle clicks for suggestions
-$("ul.geonames_input_suggestion_list li").live("click", function(){
-	$(this).parent().parent().find("input.geonames_input").val($(this).text());
-	$(this).parent().slideUp();
-});
-
-//jQuery Plugin + Timeout for Typing
 (function($) { 
-	$.fn.geoname = function(delay, locale){
-		var self = this;
-		$(this).wrap("<div class='geonames_input_holder'/>").addClass("geonames_input");
-		$(this).parent().append("<ul id='" + $(this).attr("id") + "_suggestion_list' class='geonames_input_suggestion_list'></ul>");
-		$("ul#" + $(this)	.attr("id") + "_suggestion_list")
-											.css("width", $(this).width())
-											.css("top", $(this).height());
-		$(this).keyup(function(){
-			clearTimeout(self.typing);
-			$("#" + self.attr("id") + "_suggestion_list").slideUp('fast');
-			self.typing = setTimeout(function(){ GetGeoNames(self.attr("id"), locale) }, delay);
+	$.fn.geoname = function(locale){
+		if (typeof locale == "undefined") {locale = "en";}
+		$(this).addClass("ui-geoname-input").wrap("<div class='ui-geoname-holder' />");
+		$(this).parent().prepend("<div class='ui-geoname-value'></div>");
+		
+		// Initial Value
+		if($(this).val().length == 0){
+			$(this).val(geoip_city() + ", " + geoip_region_name() + ", " + geoip_country_name());
+			if($(this).val().length > 0){
+      	$(this).hide();
+        $(this).parent().find(".ui-geoname-value").html($(this).val());
+			}
+  	}
+  		
+  		// Allow editing
+  		$(this).parent().find(".ui-geoname-value").click(function(){
+      	$(this).hide()
+      	$(this).parent().find(".ui-geoname-input").show().select();
+  		});
+  		
+  		// Return on blur
+  		$(this).blur(function(){
+				if($(this).val().length > 0){
+      		$(this).val( $(this).parent().find(".ui-geoname-value").html() ).hide();
+        	$(this).parent().find(".ui-geoname-value").show();
+  			}
+			});
+		
+		// Autocompletion
+		$(this).autocomplete({
+			delay: 1000,
+			source: function(request, response){
+				base_url = "http://ws.geonames.org/searchJSON?";
+				limit_param = "maxRows=5";
+				locale_param = "lang=" + locale;
+				type_param = "featureClass=P"
+				search_param = "name=" + encodeURIComponent(request.term);
+				final_url = base_url + "&" + limit_param + "&" + locale_param + "&" + type_param + "&" + search_param;
+				var results = new Array	
+				$.getJSON(final_url, function(data){
+					$.each(data.geonames, function(index, obj){
+						if(obj.adminName1.length == 0){
+							result = obj.toponymName + ", " + obj.countryName
+						} else {
+							result = obj.toponymName + ", " + obj.adminName1 + ", " + obj.countryName
+						}
+						results.push(result)
+					});
+					response(results);
+				}).error(function(){ response("") });
+			},
+      select: function(event, ui){
+      	$(this).parent().find(".ui-geoname-value").html(ui.item.value).show();
+        $(this).hide();
+      }
 		});
+		
 		return this;
 	}
 })(jQuery);
