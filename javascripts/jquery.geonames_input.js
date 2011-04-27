d0da4e4497d58b921abb0e27f34917341e259479
David Cornu <davidjcornu@gmail.com>
2011-04-27 14:58:24 -0400

    Quick tidy and fixed jsonp issue

--- javascripts/jquery.geonames_input.js
+++ javascripts/jquery.geonames_input.js
@@ -6,33 +6,42 @@
  */
 
 (function($) { 
-	$.fn.geoname = function(locale){
-		if (typeof locale == "undefined") {locale = "en";}
+  $.fn.geoname = function(locale){
+    if (typeof locale == "undefined") {locale = "en";}
 
-		$(this).autocomplete({
-			delay: 1000,
-			source: function(request, response){
-				base_url = "http://ws.geonames.org/searchJSON?";
-				limit_param = "maxRows=5";
-				locale_param = "lang=" + locale;
-				type_param = "featureClass=P"
-				search_param = "name=" + encodeURIComponent(request.term);
-				final_url = base_url + "&" + limit_param + "&" + locale_param + "&" + type_param + "&" + search_param;
-				var results = new Array	
-				$.getJSON(final_url, function(data){
-					$.each(data.geonames, function(index, obj){
-						if(obj.adminName1.length == 0){
-							result = obj.toponymName + ", " + obj.countryName
-						} else {
-							result = obj.toponymName + ", " + obj.adminName1 + ", " + obj.countryName
-						}
-						results.push(result)
-					});
-					response(results);
-				}).error(function(){ response("") });
-			}
-		});
-		
-		return this;
-	}
+    $(this).autocomplete({
+      delay: 1000,
+      source: function(request, response){
+        var search_url = "http://ws.geonames.org/searchJSON?";
+        var params = {
+					maxRows: 5,
+					lang: locale,
+					featureClass: "P",
+					name: encodeURIComponent(request.term)
+				}	
+        var results = new Array 
+        $.ajax({
+					url: search_url + $.param(params),
+					method: "GET",
+					dataType: "jsonp",
+					success: function(data){
+	          $.each(data.geonames, function(index, obj){
+	            if(obj.adminName1.length == 0){
+	              var result = obj.toponymName + ", " + obj.countryName
+	            } else {
+	              var result = obj.toponymName + ", " + obj.adminName1 + ", " + obj.countryName
+	            }
+	            results.push(result)
+	          });
+	          response(results);
+	        },
+					error: function(){
+						response("");
+					}
+				});
+      }
+    });
+
+    return this;
+  }
 })(jQuery);
